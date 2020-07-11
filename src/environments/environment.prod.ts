1b8e5a05536721bb0d80bc59c1f6c5545a466baf
rajatgangwani33@gmail.com <rajatgangwani33@gmail.com>
2020-07-11 14:54:30 +0530

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true
-};
-
-export const Urls = {
-  wrapper_img_path: './assets/Images/inspect/Front-End/Page 1/',
-  wrapperChild_img_path: './assets/Images/inspect/Front-End/Page 1/',
-};
